c09dbd56a09656b7d177af29c3f0e452dfdbf317
Skillful Li <lwangluoab@163.com>
2024-03-18 23:25:52 +0800

    实验

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -2,7 +2,7 @@ export const redirects = JSON.parse("{}")
 
 export const routes = Object.fromEntries([
   ["/contributing.html", { loader: () => import(/* webpackChunkName: "contributing.html" */"F:/CODE__CODE/gentlelyyli.github.io/gentlelyyli.github.io/docs/.vuepress/.temp/pages/contributing.html.js"), meta: {"title":""} }],
-  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"F:/CODE__CODE/gentlelyyli.github.io/gentlelyyli.github.io/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"gentlelyyli.github.io"} }],
+  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"F:/CODE__CODE/gentlelyyli.github.io/gentlelyyli.github.io/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":""} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"F:/CODE__CODE/gentlelyyli.github.io/gentlelyyli.github.io/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
 
--- docs/.vuepress/.temp/pages/index.html.vue
+++ docs/.vuepress/.temp/pages/index.html.vue
@@ -1,5 +1,5 @@
-<template><div><h1 id="gentlelyyli-github-io" tabindex="-1"><a class="header-anchor" href="#gentlelyyli-github-io"><span>gentlelyyli.github.io</span></a></h1>
-<p>Blog</p>
+<template><div><p>#baisir 博客的搭建
+Blog</p>
 </div></template>
 
 
