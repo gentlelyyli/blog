9e598fa67b7c9f98bfffaa8df7c090462aea4a3a
Skillful Li <lwangluoab@163.com>
2024-03-18 22:41:31 +0800

    实验

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,8 +5,4 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   bundler: viteBundler(),
   theme: defaultTheme(),
-
-  lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
 })
