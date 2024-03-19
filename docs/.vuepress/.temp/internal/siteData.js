e61ebafe6016182dbf87405149d160c1e3ab6982
Skillful Li <lwangluoab@163.com>
2024-03-19 12:12:14 +0800

    test

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,4 +1,4 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"白先生神圣不容侵犯！！\",\"description\":\"这是我的第一个 VuePress 站点\",\"head\":[],\"locales\":{}}")
+export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"白先生神圣不容侵犯！！\",\"description\":\"这是我的第一个 VuePress 站点\",\"head\":[[\"link\",{\"rel\":\"icon\",\"herf\":\"/1.jpg\"}]],\"locales\":{}}")
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,11 +2,77 @@ import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
-export default defineUserConfig({
+export default ({
   bundler: viteBundler(),
-  theme: defaultTheme(),
+ // theme: defaultTheme(),
 
   lang: 'zh-CN',
   title: '白先生神圣不容侵犯！！',
   description: '这是我的第一个 VuePress 站点',
+  head:[['link',{rel:'icon',herf:'/1.jpg'}]],
+
+  theme: defaultTheme({
+    navbar:[
+      {
+      text:'首页',
+      link:'/',
+      },
+
+      {
+        text: 'css',
+        children: ['/pages/css/css01.md', '/pages/css/css02.md'],
+      },
+
+
+
+      {
+        text: 'js',
+        children: [
+          {
+            text: 'js基础',
+            children: ['/pages/js/js01.md', '/pages/js/js02.md'],
+          },
+          {
+            text: 'js高级',
+            children: ['/pages/js/js03.md'],
+          },
+        ],
+      }
+
+
+      
+
+    ],
+
+    sidebar: {
+      '/pages/js/': [
+        {
+          text: 'js1',
+          collapsible: true, // 可折叠
+          children: ['/pages/js/js01.md', '/pages/js/js02.md'],
+        },
+        {
+          text: 'js2',
+          collapsible: true,
+          children: ['/pages/js/js03.md'],
+        },
+      ],
+      '/pages/css/': [
+        {
+          text: 'css',
+          collapsible: true,
+          children: ['/pages/css/css01.md', '/pages/css/css02.md'],
+        },
+      ],
+      '/pages/html/': [
+        {
+          text: 'html',
+          collapsible: true,
+          children: ['/pages/html/html01.md', '/pages/html/html02.md'],
+        },
+      ],
+    },
+
+
+  }),
 })
